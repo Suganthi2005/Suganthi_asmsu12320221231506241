7c475b291fc9ee1ccfb2074fa969c7e5be9c8ec3
Suganthi2005 <144357271+Suganthi2005@users.noreply.github.com>
2023-09-07 20:43:20 +0530

    Add files via upload

---
+++ year.py
@@ -0,0 +1,15 @@
+#implement a recursive function to calculate the factorial of a given number# 
+def factorial(n):
+    if n < 0:
+        return 0
+    elif n == 0 or n == 1: 
+    else:
+        fact = 1
+        while(n > 1):
+            fact *= n
+            n -= 1
+        return fact
+
+num = 5
+print("Factorial of",num,"is",
+factorial(num))
